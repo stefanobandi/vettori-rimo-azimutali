7162909a7755b91d02a76e5791367a9262c9cf10
stefanobandi <stefano.bandi22@gmail.com>
2025-12-17 23:25:06 +0100

    Update visualization.py

--- visualization.py
+++ visualization.py
@@ -14,7 +14,7 @@ def plot_clock(azimuth_deg, color):
     fig.patch.set_alpha(0)
     return fig
 
-def draw_static_elements(ax, pos_sx, pos_dx, rad1, rad2):
+def draw_static_elements(ax, pos_sx, pos_dx):
     # Scafo
     hw, stern, bow_tip, shoulder = 5.85, -16.25, 16.25, 8.0
     path_data = [
@@ -31,10 +31,6 @@ def draw_static_elements(ax, pos_sx, pos_dx, rad1, rad2):
     f_codes, f_verts = zip(*fender_data)
     ax.add_patch(PathPatch(Path(f_verts, f_codes), facecolor='none', edgecolor='#333333', lw=8, capstyle='round', zorder=2))
 
-    # Cerchi Azimutali (I "pezzi" che mancavano)
+    # Cerchi Azimutali
     ax.add_patch(plt.Circle(pos_sx, 2.0, color='black', fill=False, lw=1.5, ls='-', alpha=0.6, zorder=2))
     ax.add_patch(plt.Circle(pos_dx, 2.0, color='black', fill=False, lw=1.5, ls='-', alpha=0.6, zorder=2))
-
-    # Suggerimento eliche
-    ax.plot([pos_sx[0], pos_sx[0] + 2.0 * np.sin(rad1)], [pos_sx[1], pos_sx[1] + 2.0 * np.cos(rad1)], color='black', lw=2, zorder=3)
-    ax.plot([pos_dx[0], pos_dx[0] + 2.0 * np.sin(rad2)], [pos_dx[1], pos_dx[1] + 2.0 * np.cos(rad2)], color='black', lw=2, zorder=3)
