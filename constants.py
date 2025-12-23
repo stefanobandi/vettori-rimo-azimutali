7783046ff7ddd5ab92ce2d56c5662eda41666f5d
stefanobandi <stefano.bandi22@gmail.com>
2025-12-23 15:27:09 +0100

    Update constants.py

--- constants.py
+++ constants.py
@@ -1,5 +1,4 @@
-# constants.py
-G_ACCEL = 9.80665  # Accelerazione gravità
+G_ACCEL = 9.80665  # Accelerazione gravità per conversione tonnellate -> kN
 POS_THRUSTERS_Y = -12.0
 POS_THRUSTERS_X = 2.7
-BOLLARD_PULL_PER_ENGINE = 35.0 # Tonnellate
+BOLLARD_PULL_PER_ENGINE = 35.0 # Tonnellate (Bollard Pull totale 70t)
