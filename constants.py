e0b1083fa92e6945eeec4066ff2447d589952ba2
stefanobandi <stefano.bandi22@gmail.com>
2025-12-23 22:21:05 +0100

    Update constants.py

--- constants.py
+++ constants.py
@@ -8,8 +8,7 @@ MASS = 700000.0  # 700 tonnellate in kg
 # Momento d'inerzia stimato per un corpo di 32.5m x 11.7m
 IZ = 0.08 * MASS * (32.5**2 + 11.7**2) 
 
-# Coefficienti di smorzamento (Damping) per raggiungere le velocità limite indicate
-# Calcolati per equilibrare la spinta massima alle velocità di regime
+# Coefficienti di smorzamento (Damping)
 DAMPING_LINEAR_X = 110000.0  # Per ~12.5 nodi avanti
-DAMPING_LINEAR_Y = 850000.0  # Per ~1.5-2 nodi laterali (resistenza molto maggiore)
+DAMPING_LINEAR_Y = 850000.0  # Per ~1.5-2 nodi laterali
 DAMPING_ANGULAR = 50000000.0 # Per stabilizzare la rotazione
