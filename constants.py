e6d9d8342f87006a7ee7cea03fb18b8cb2edac92
Stefano Bandi <stefano.bandi22@gmail.com>
2026-01-03 21:23:17 +0100

    Update constants.py

--- constants.py
+++ constants.py
@@ -6,29 +6,21 @@ POS_THRUSTERS_Y = -12.0
 POS_THRUSTERS_X = 2.7
 BOLLARD_PULL_PER_ENGINE = 35.0 
 
-# --- FISICA V6.5 (Adaptive Pivot Logic) ---
+# --- FISICA V6.5 ---
 MASS = 700000.0 
 
 # Resistenza Longitudinale
-# Mantiene la velocità sotto controllo durante le manovre
 K_Y = 25000.0       
 
 # --- RESISTENZE LATERALI BASE ---
-# Questi sono i valori "di crociera".
-# Vengono modificati dinamicamente in physics.py in base alla manovra.
-
-# Resistenza Prua Base (Skeg)
+# Valori medi, saranno scalati dinamicamente
 K_X_BOW = 500000.0     
-
-# Resistenza Poppa (Scafo)
-# Costante. È il nostro riferimento fisso.
 K_X_STERN = 120000.0    
 
 # Coordinate Centri di Pressione
 Y_BOW_CP = 13.0       
 Y_STERN_CP = -11.0    
 
-# Resistenza Rotazionale (Rotational Damping)
-# Calibrato per 180 gradi in 30 sec a piena potenza
+# Resistenza Rotazionale
 K_W = 5.0e7         
 I_Z = 65000000.0
