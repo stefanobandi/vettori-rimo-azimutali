45aac0c0076634ac43f2794452dd3dff6a0b5de7
Stefano Bandi <stefano.bandi22@gmail.com>
2026-01-04 22:20:06 +0100

    Update constants.py

--- constants.py
+++ constants.py
@@ -12,17 +12,17 @@ BOLLARD_PULL_PER_ENGINE = 35.0
 SHIP_LENGTH = 32.50
 SHIP_WIDTH = 11.70
 
-# --- FISICA "BRICK ON ICE" (Experimental V6.60 - Agile Rotation V2) ---
-MASS = 800000.0          # kg (Massa invariata: mantiene la pesantezza in traslazione)
+# --- FISICA "BRICK ON ICE" (Experimental V6.60 - Balanced) ---
+MASS = 800000.0          # kg (Massa invariata)
 
-# PRIMA: 60.000.000 -> ORA: 50.000.000
-# Scatto iniziale sulla rotazione più rapido
-INERTIA = 50000000.0     
+# RIPRISTINATA: 60.000.000
+# Un po' più di resistenza all'avvio per dare "corposità" alla manovra
+INERTIA = 60000000.0     
 
 # --- DAMPING (Freno Idrodinamico) ---
 DAMP_LINEAR_X = 30000.0  # Freno laterale (Scarroccio)
 DAMP_LINEAR_Y = 8000.0   # Freno longitudinale (Avanzamento)
 
-# PRIMA: 25.000.000 -> ORA: 12.000.000
-# Freno rotazionale molto basso: ora la nave gira "libera" e veloce
+# MANTENUTO BASSO: 12.000.000
+# Una volta partita, la nave gira libera e veloce
 DAMP_ANGULAR = 12000000.0
