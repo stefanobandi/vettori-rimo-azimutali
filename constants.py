3f9752a5963fec181628546e479396d44b34e802
Stefano Bandi <stefano.bandi22@gmail.com>
2026-01-04 21:27:55 +0100

    Update constants.py

--- constants.py
+++ constants.py
@@ -6,15 +6,18 @@ POS_THRUSTERS_Y = -12.0
 POS_THRUSTERS_X = 2.7
 BOLLARD_PULL_PER_ENGINE = 35.0 
 
-# --- FISICA "BRICK ON ICE" (Experimental) ---
-# Calibrazione per ASD 2810 (circa)
-# Massa aumentata (Added Mass) per simulare l'acqua spostata
-MASS = 850000.0          # kg (Dislocamento + Massa aggiunta)
-INERTIA = 100000000.0    # kg*m^2 (Inerzia rotazionale molto alta per evitare trottole)
+# Dati Nave (Visualizzazione & UI)
+SHIP_LENGTH = 28.0
+SHIP_WIDTH = 11.0
 
-# --- DAMPING (Freno Idrodinamico Semplificato) ---
-# Questi valori frenano il "ghiaccio". 
-# Più alti sono, più la nave è "immersa nella melassa".
-DAMP_LINEAR_X = 15000.0  # Resistenza allo scarroccio laterale
-DAMP_LINEAR_Y = 5000.0   # Resistenza all'avanzamento (minore dello scarroccio)
-DAMP_ANGULAR = 25000000.0 # Resistenza alla rotazione (molto alta per stabilità)
+# --- FISICA "BRICK ON ICE" (Experimental V6.60) ---
+# Calibrazione per ASD
+MASS = 800000.0          # kg (Dislocamento + Massa aggiunta)
+INERTIA = 120000000.0    # kg*m^2 (Inerzia aumentata per movimenti più maestosi)
+
+# --- DAMPING (Freno Idrodinamico) ---
+# Resistenza proporzionale alla velocità (Viscosità)
+# Impedisce alla nave di accelerare all'infinito
+DAMP_LINEAR_X = 25000.0  # Freno laterale (Scarroccio) - Più alto perché lo scafo fa muro
+DAMP_LINEAR_Y = 8000.0   # Freno longitudinale (Avanzamento) - Più basso, scafo idrodinamico
+DAMP_ANGULAR = 40000000.0 # Freno rotazionale - Fondamentale per fermare la trottola
