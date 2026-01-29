34767eeaf2c79cb1c09b558661d13ea79bce353f
Stefano Bandi <stefano.bandi22@gmail.com>
2026-01-29 16:04:18 +0100

    Update constants.py

--- constants.py
+++ constants.py
@@ -7,31 +7,30 @@ G_ACCEL = 9.80665
 SHIP_LENGTH = 32.50
 SHIP_WIDTH = 11.70
 
-# Posizioni Geometriche (V6.62 + V7.1 Physics)
+# Posizioni Geometriche
 POS_THRUSTERS_Y = -12.0
-POS_THRUSTERS_X = 2.7  # Usato per la grafica V6.62
-POS_SKEG_Y = 5.0       # V7.1: Punto di resistenza prua
-POS_STERN_Y = -12.0    # V7.1: Punto di spinta poppa
+POS_THRUSTERS_X = 2.7
+POS_SKEG_Y = 5.0       # Punto di resistenza prua
+POS_STERN_Y = -12.0    # Punto di spinta poppa
 
 # Parametri Motori
 BOLLARD_PULL_PER_ENGINE = 35.0 
 MAX_THRUST = 750000.0   # Newton (approx 75 ton)
 
-# --- FISICA "HEAVY & STABLE" (V7.1 Tuning) ---
-SHIP_MASS = 800000.0            # 800 Tonnellate
-MOMENT_OF_INERTIA = 100000000.0 # Altissima inerzia
+# --- FISICA (V7.1 Tuning) ---
+SHIP_MASS = 800000.0            
+MOMENT_OF_INERTIA = 100000000.0 
 
-# Damping (Resistenza Idrodinamica)
+# Damping
 LINEAR_DAMPING_SURGE = 50000.0
-LINEAR_DAMPING_SWAY = 350000.0  # Alto per effetto Skeg
-ANGULAR_DAMPING = 30000000.0    # Alto per stabilità rotta
+LINEAR_DAMPING_SWAY = 350000.0 
+ANGULAR_DAMPING = 30000000.0    
 
-# Offset propulsori per calcolo fisico (coincidono con grafica)
+# Offset
 THRUSTER_X_OFFSET = POS_THRUSTERS_X
 THRUSTER_Y_OFFSET = POS_THRUSTERS_Y
 
-# --- VISUALIZZAZIONE ---
+# Visualizzazione
 SCREEN_WIDTH = 1200
 SCREEN_HEIGHT = 800
-PIXELS_PER_METER = 10
 COLOR_SEA = (20, 30, 40)
