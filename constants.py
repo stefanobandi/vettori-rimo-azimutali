25c2e652101ec447a555c81b9ff63ce8c5d4a241
Stefano Bandi <stefano.bandi22@gmail.com>
2026-01-29 16:26:09 +0100

    Update constants.py

--- constants.py
+++ constants.py
@@ -3,27 +3,29 @@ import numpy as np
 # --- DIMENSIONI & GEOMETRIA ---
 G_ACCEL = 9.80665
 
-# Dati Nave Reali
+# Dati Nave
 SHIP_LENGTH = 32.50
 SHIP_WIDTH = 11.70
 
 # Posizioni Geometriche
 POS_THRUSTERS_Y = -12.0
 POS_THRUSTERS_X = 2.7
-POS_SKEG_Y = 5.0       # Punto di resistenza prua
-POS_STERN_Y = -12.0    # Punto di spinta poppa
+POS_SKEG_Y = 5.0       # Target per Fast Side Step
+POS_STERN_Y = -12.0    
 
 # Parametri Motori
 BOLLARD_PULL_PER_ENGINE = 35.0 
-MAX_THRUST = 750000.0   # Newton (approx 75 ton)
+MAX_THRUST = 735000.0   # ~75 tonnellate forza
 
-# --- FISICA (V7.1 Tuning) ---
+# --- FISICA (Tuning 12.7 kt) ---
 SHIP_MASS = 800000.0            
 MOMENT_OF_INERTIA = 100000000.0 
 
-# Damping
-LINEAR_DAMPING_SURGE = 50000.0
-LINEAR_DAMPING_SWAY = 350000.0 
+# Calcolo Damping per V_max = 12.7 kt (6.5 m/s)
+# F_tot = 2 * MAX_THRUST = 1,470,000 N
+# Damping = F / V = 1,470,000 / 6.5 = ~226,000
+LINEAR_DAMPING_SURGE = 226000.0
+LINEAR_DAMPING_SWAY = 650000.0   # Più alto per deriva laterale
 ANGULAR_DAMPING = 30000000.0    
 
 # Offset
